e3ec6837b4969560bc3954404abf7fc1fcc062a0
Om Preetham <ompreethambandi@gmail.com>
2024-04-05 17:17:35 -0500

    designed two sections with scss

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+const Navigation = () => {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__list__item">
+          <a href="#">Home</a>
+        </li>
+        <li className="nav__list__item">
+          <a href="#">New</a>
+        </li>
+        <li className="nav__list__item">
+          <a href="#">Popular</a>
+        </li>
+        <li className="nav__list__item">
+          <a href="#">Trending</a>
+        </li>
+        <li className="nav__list__item">
+          <a href="#">Categories</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+export default Navigation
--- src/components/TopStory.jsx
+++ src/components/TopStory.jsx
@@ -1,23 +1,25 @@
 const TopStory = () => {
   return (
-    <section className="top-story-wrapper">
-      <article className="top-story">
-        <div className="top-story__image-wrapper">
+    <section className="top-story top-story-wrapper">
+      <article className="top-story__article">
+        <div className="top-story__article__image-wrapper">
           <img
-            className="top-story__image"
+            className="top-story__article__image"
             src="/images/image-web-3-desktop.jpg"
             alt="image-web-3-desktop"
           />
         </div>
-        <div className="top-story__content">
-          <h2 className="top-story__heading">The Bright Future of Web 3.0?</h2>
-          <div className="top-story__description">
-            <p className="top-story__summary">
+        <div className="top-story__article__content">
+          <h2 className="top-story__article__heading">
+            The Bright Future of Web 3.0?
+          </h2>
+          <div className="top-story__article__description">
+            <p className="top-story__article__summary">
               We dive into the next evolution of the web that claims to put the
               power of the platforms back into the hands of the people. But is
               it really fulfilling its promise?
             </p>
-            <button className="top-story__read-more">Read more</button>
+            <button className="top-story__article__read-more">Read more</button>
           </div>
         </div>
       </article>
